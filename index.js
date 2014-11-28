989781a6dd22d543291fee120d1cee11f416160c
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-28 15:07:20 +0100

    Creating files for a new feature

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+var fs      = require ('fs'),
+    path    = require('path'),
+    mkdirp  = require('mkdirp'),
+    feature = process.argv[2];
+
+// creating feature root directory
+mkdirp(feature, handleCreationError);
+
+// creating angular-module
+fs.writeFile(feature + '/' + feature + 'module.js', 'module', handleCreationError);
+
+// creating route configuration of module
+fs.writeFile(feature + '/' + feature + 'routes.js', 'routes', handleCreationError);
+
+// creating controller of module
+fs.writeFile(feature + '/' + feature + '.js', 'controller', handleCreationError);
+
+// creating html template of module
+fs.writeFile(feature + '/' + feature + 'html', 'template', handleCreationError);
+
+function handleCreationError(error) {
+  if (error) console.log(error);
+}
