c53be8d7cf6fbafdc80a3598975e535be20ab2db
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-29 20:34:44 +0100

    Implementing function to check if directory is a git-repository

--- test/git.specs.js
+++ test/git.specs.js
@@ -1,13 +1,30 @@
 'use strict';
 
-var assert = require('should');
+var git    = require('../lib/git'),
+    should = require('should');
 
 describe('Git handle branching', function() {
 
   describe('If no git repository is initialized', function() {
 
+    var projectDir = process.cwd();
+    var toNoGitRepository    = '../';
+
+    before(function() {
+
+      process.chdir(toNoGitRepository);
+    });
+
+    after(function() {
+
+      process.chdir(projectDir);
+    });
+
     it('should not create a branch', function() {
-      true.should.be.true;
+      git.isRepo(function(isRepo) {
+
+        isRepo.should.be.false;
+      });
     });
   });
 });
