ba92a46b61e90ccd30dbc7076a3d5885d0f069cf
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-29 18:47:34 +0100

    Moving function parse out of module declaration

---
+++ test/git.specs.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var assert = require('should');
+
+describe('Git handle branching', function() {
+
+  describe('If no git repository is initialized', function() {
+
+    it('should not create a branch', function() {
+      true.should.be.true;
+    });
+  });
+});
