288f6dc233d869e84b64279294e2c5779dde4068
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-29 21:57:53 +0100

    Switchung mocha reporter to spec

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var gulp  = require('gulp'),
     mocha = require('gulp-mocha');
 
 gulp.task('default', function() {
-  
+
   return gulp
     .src('test/*specs.js', { read:false })
-    .pipe(mocha({ reporter: 'nyan' }));
+    .pipe(mocha({ reporter: 'spec' }));
 });
