d3b5e0cd9591966c02e719a5340246ab414a8af4
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-29 15:46:09 +0100

    Adding module parsing the name of the feature

---
+++ test/feature.specs.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var feature = require('../lib/feature');
+var should = require('should');
+
+describe('Checking command line argument for name of feature', function() {
+
+  describe('If no feature name is given', function() {
+
+    var argv = undefined;
+
+    it('should throw an error', function() {
+
+      (function() {
+
+        feature.parse(argv);
+      })
+        .should
+        .throw('Please specify a valid name for your feature!')
+    });
+  });
+
+  describe('If a valid feature name is given', function() {
+
+    var argv = ['_', 'app-path/', 'feature-a'];
+
+    it('should return the name of the parsed feature name', function() {
+
+      feature.parse(argv).should.eql('feature-a');
+    });
+  });
+});
