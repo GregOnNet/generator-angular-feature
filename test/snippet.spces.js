a658b1b705083f507858e54baea2fe287ac23d4e
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-30 13:11:50 +0100

    Processing first snippet with handlbars

--- test/snippet.spces.js
+++ test/snippet.spces.js
@@ -1,18 +1,26 @@
 'use strict';
 
-var fs     = require('fs'),
-    should = require('should');
+var fs         = require('fs'),
+    should     = require('should'),
+    Handlebars = require('handlebars');
 
 describe('Loading snippet templates', function() {
 
   describe('When loading a snippet file', function() {
 
+    var featureName = { 'feature' : 'login' };
+    var options = { encoding: 'utf-8' };
+    var template, result;
+
     it('should return its content', function() {
-      var options = { encoding: 'utf-8' };
+
       fs.readFile('snippets/feature.js', options, function(error, data) {
         if (error) throw error;
 
-        console.log(data);
+        template = Handlebars.compile(data);
+        result   = template(featureName);
+
+        result.should.containEql('login');
       });
     });
   });
